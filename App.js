affe80d93adc71dcb3679fae27c4c91e83333237
3loomin <3loomin@gmail.com>
2022-03-03 00:00:04 +0900

    내 마음대로 예제 바꾸기! usestate(false)로 바꾸고 상호작용 되게 수정했음!

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Button, Text, View } from "react-native";
 
 const Cat = (props) => {
-  const [isHungry, setIsHungry] = useState(true);
+  const [isHungry, setIsHungry] = useState(false);
   //useState( 여기 ) 여기에는 문자열, 숫자, 부울, 배열, 개체 등 모든 종류의 데이터를 추적하는 데 사용할 수 있습니다 . 예를 들어 고양이를 쓰다듬어준 횟수를 추적할 수 있습니다 const [timesPetted, setTimesPetted] = useState(0)!
   return (
     <View>
@@ -10,14 +10,14 @@ const Cat = (props) => {
       <Text></Text>
       <Text></Text>
       <Text>
-        I am {props.name}, and I am {isHungry ? "hungry" : "full"}!
+        I am {props.name}, and I am {isHungry ? "full" : "hungry" }!
       </Text>
       <Button
         onPress={() => {
-          setIsHungry(false);
+          setIsHungry(true);
         }}
-        disabled={!isHungry}
-        title={isHungry ? "Pour me some milk, please!" : "Thank you!"}
+        disabled={isHungry}
+        title={isHungry ?  "Thank you!" : "Pour me some milk, please!"}
       />
     </View>
   );
