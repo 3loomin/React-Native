56f7ba409168631f89e6db20e62a7b838ae1d040
3loomin <3loomin@gmail.com>
2022-03-02 23:24:37 +0900

    Core Components and Native Components

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import React from "react";
-import { Text, View } from 'react-native';
+import { Text, View, Image, ScrollView, TextInput } from 'react-native';
 
-const HelloWorldApp = () => {
+const App = () => {
   return(
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <Text>Hello, world!</Text>
-    </View>
+    <ScrollView>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text>어쩌구가 저쩌구</Text>
+      <View>
+        <Text>저쩌구가 어쩌구 했대요! </Text>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style = {{ width: 200, height: 200}}
+          />
+      </View>
+      <TextInput
+        style={{
+          height:40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue = "야호야호 이것이 박스에오에오"
+        />
+    </ScrollView>
   );
 }
-export default HelloWorldApp;
+export default App;
