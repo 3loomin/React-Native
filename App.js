da5cd5b0c2c2479aeabc336e9371aa97dad9fc73
3loomin <3loomin@gmail.com>
2022-03-02 22:39:50 +0900

    Third React-Native Tutorial

--- App.js
+++ App.js
@@ -1,28 +1,50 @@
-import React from 'react';
-import { Text, View,  StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { 
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native';
 
-const style = StyleSheet.create({
-  center: {
-    alignItems: 'center'
+class App extends Component {
+  state = {
+    count : 0
   }
+  onPress = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  render(){
+    return(
+      <View style = {styles.container}>
+        <TouchableOpacity
+          style = {styles.button}
+          onPress = {this.onPress}
+        >
+          <Text>Click Me!</Text>
+        </TouchableOpacity>
+        <View>
+          <Text>
+            You clicked { this.state.count } times
+          </Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10
+   }
 })
-
-const Greeting = (props) => {
-  return(
-    <View style = {style.center}>
-      <Text>Hello {props.name}!!!</Text>
-    </View>
-  );
-}
-
-const LotsGreetings = () => {
-  return(
-    <View style = {[style.center,{top:50}]}>
-      <Greeting name = 'RExxar'/>
-      <Greeting name = 'Jaina'/>
-      <Greeting name = 'Valeera'/>
-    </View>
-  );
-}
-
-export default LotsGreetings;
+export default App;
