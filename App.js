ec9b8a6e412d96ef1d221860f064160f13967f4c
3loomin <3loomin@gmail.com>
2022-03-02 23:55:56 +0900

    React Fundamental -State

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Button, Text, View } from "react-native";
 
 const Cat = (props) => {
   const [isHungry, setIsHungry] = useState(true);
-
+  //useState( 여기 ) 여기에는 문자열, 숫자, 부울, 배열, 개체 등 모든 종류의 데이터를 추적하는 데 사용할 수 있습니다 . 예를 들어 고양이를 쓰다듬어준 횟수를 추적할 수 있습니다 const [timesPetted, setTimesPetted] = useState(0)!
   return (
     <View>
       <Text></Text>
